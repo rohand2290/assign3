6e6b013fcb0b37370afb25c3c273fd540f7e896b
Rohan Deshpande <rohandeshpande@Rohans-MacBook-Air.local>
2026-04-22 17:30:08 -0700

    finished tic tac toe implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
 import * as ReactBootstrap from 'react-bootstrap'
-import Board from './Board'
+import Game from './Board'
 const { Badge, Button, Card } = ReactBootstrap
 
 export default function App() {
   const [name, setName] = React.useState('World')
 
   return (
-    <div>
-      <Board />
-    </div>
+      <Game />
   )
 }
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,16 +1,50 @@
 import { useState } from "react";
 import Square from "./Square";
 
+let calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 
-let Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null))
+}
+
+
+let Board = ({xIsNext, squares, onPlay}) => {
     let handleClick = (i) => {
+        if (squares[i] || calculateWinner(squares)) return;
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
-        setSquares(nextSquares);
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        onPlay(nextSquares)
+    }
+    
+    const winner = calculateWinner(squares)
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O")
     }
     return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -30,4 +64,46 @@ let Board = () => {
     )
 }
 
-export default Board;
+const Game = () => {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    const handlePlay = (nextSquares) => {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    const jumpTo = (nextMove) => {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        )
+    })
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    )
+}
+
+export default Game;
